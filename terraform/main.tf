873feb8bc7e3feca2ee513ef4f7d1f5f742f387b
Eric Ola-Douglas <octesign@gmail.com>
2024-09-20 14:54:31 +0100

    second commit

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+#provider block to specify desired cloud provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.68.0"
+    }
+  }
+}
+
+#configuring the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#provision vpc resource - aws_vpc
+resource "aws_vpc" "easy-school" {
+  cidr_block       = "192.168.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "easy-school"
+  }
+}
+
+#create ec2 instance 
+resource "aws_instance" "easy-sec" {
+  ami = ""
+  instance_type = " "
+
+  tags = {
+    Name = "easy-sec"
+  }
+}
