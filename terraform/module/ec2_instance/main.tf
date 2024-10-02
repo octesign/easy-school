548a79483074d01b492c03a0d12d52c7df6222ec
Eric Ola-Douglas <octesign@gmail.com>
2024-10-02 16:51:32 +0100

    terraform upd3

--- terraform/module/ec2_instance/main.tf
+++ terraform/module/ec2_instance/main.tf
@@ -3,7 +3,22 @@
 #   region = "us-east-1"
 # }
 
-#create ec2 instances in the public subnets
+#create ec2 instances in the public subnet 1
+resource "aws_instance" "easy-pub" {
+  count = var.instance_count_value_sec
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  key_name = module.key_pair.easy_access.key_name
+  subnet_id = path.module.subnets.easy_public_subnet_2.id
+  security_groups = [module.sg.public_security_group_id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "easy-pub-${count.index + 1}"
+  }
+}
+
+#create ec2 instances in the public subnet 2
 resource "aws_instance" "easy-sec" {
   count = var.instance_count_value_sec
   ami           = var.ami_value
@@ -14,11 +29,26 @@ resource "aws_instance" "easy-sec" {
   associate_public_ip_address = true
 
   tags = {
-    Name = "easy-sec"
+    Name = "easy-sec-${count.index + 1}"
   }
 }
 
-#create ec2 instances in the private subnets
+#create ec2 instances in the private subnet 1
+resource "aws_instance" "easy-back" {
+  count = var.instance_count_value_prim
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  key_name = module.key_pair.easy_access.key_name
+  subnet_id = module.subnets.easy_private_subnet_2_id
+  security_groups = [module.sg.private_security_group_id]
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "easy-back-${count.index + 1}"
+  }
+}
+
+#create ec2 instances in the private subnet 2
 resource "aws_instance" "easy-prim" {
   count = var.instance_count_value_prim
   ami = var.ami_value
@@ -29,7 +59,7 @@ resource "aws_instance" "easy-prim" {
   associate_public_ip_address = false
 
   tags = {
-    Name = "easy-sec"
+    Name = "easy-prim-${count.index + 1}"
   }
 }
 
--- terraform/module/key_pair/output.tf
+++ terraform/module/key_pair/output.tf
@@ -1,5 +1,5 @@
 output "key_pair_id" {
-  value = aws_key_pair.easy_access.id
+  value = aws_key_pair.easy_access.key_pair_id
 }
 
 output "key_pair_fingerprint" {
@@ -8,4 +8,8 @@ output "key_pair_fingerprint" {
 
 output "key_pair_public_key" {
   value = aws_key_pair.easy_access.public_key
+}
+
+output "key_pair_name" {
+  value = aws_key_pair.easy_access.key_name
 }
